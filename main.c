9594265556f76a799be9e6b06e23df19859520f3
Belle <79115662+Isabelle-Matos@users.noreply.github.com>
2023-05-22 10:59:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -20,21 +20,41 @@ int main(){
                                                                 pois a pilha não possui memória suficiente para guardar
                                                                 200000 vetor_heapes de struct*/
     
-    Registro vetor1[20];
-    gerar_vetor_reg_aleatorio(vetor_heap, 200000);
+ 
+    //gerar_vetor_reg_aleatorio(vetor_heap, 200000);
     // gerar_vetor_reg_crescente(vetor_heap, 20);
-    // gerar_vetor_inteiros_crescente(vetor, 20);
+    gerar_vetor_inteiros_crescente(vetor, 20);
     // gerar_vetor_inteiros_decrescente(vetor, 20);
-    //gerar_vetor_reg_decrescente(vetor1, 20);
+    //gerar_vetor_reg_decrescente(vetor, 20);
     
  
     gettimeofday(&start, NULL);
-    selectionSort_regrande(vetor_heap, 200000, &comp, &desloc);
+    //selectionSort_regrande(vetor_heap, 200000, &comp, &desloc);
+    selectionSort_regpequeno(vetor, 20, &comp, &desloc);
     gettimeofday(&end, NULL);
     //   for(int i=0; i<20; i++){
-    //      printf("ordenado: %d\n", vetor1[i].chave);
+    //      printf("ordenado: %d\n", vetor[i].chave);
     //   }
+    printf("Selection sort registros pequenos\n");
+    startTime = start.tv_sec + start.tv_usec / 1000000.0L;
+    endTime = end.tv_sec + end.tv_usec / 1000000.0L;
+    printf("Tempo inicial = %Lf\n", startTime);
+    printf("Tempo final = %Lf\n", endTime);
+    printf("Tempo decorrido = %Lf\n", endTime - startTime);
+    printf("Comparações = %ld\n", comp);
+    printf("Tempo decorrido = %ld\n", desloc);
     
+    printf("\nQuick sort registros pequenos\n");
+    comp = 0;
+    desloc = 0;
+    gerar_vetor_inteiros_decrescente(vetor, 20);
+    gettimeofday(&start, NULL);
+    quickSort_pequeno(vetor, 0, 19, &comp, &desloc);
+    gettimeofday(&end, NULL);
+    //   for(int i=0; i<20; i++){
+    //      printf("ordenado: %d\n", vetor[i].chave);
+    //   }
+    //free(vetor_heap);
     startTime = start.tv_sec + start.tv_usec / 1000000.0L;
     endTime = end.tv_sec + end.tv_usec / 1000000.0L;
     printf("Tempo inicial = %Lf\n", startTime);
@@ -43,14 +63,13 @@ int main(){
     printf("Comparações = %ld\n", comp);
     printf("Tempo decorrido = %ld\n", desloc);
 
-    gerar_vetor_reg_aleatorio(vetor_heap, 200000);
+    printf("\nInsertion sort registros pequenos\n");
+    comp = 0;
+    desloc = 0;
+    gerar_vetor_inteiros_rand(vetor, 20);
     gettimeofday(&start, NULL);
-    quickSort_grande(vetor_heap, 0, 199999, &comp, &desloc);
+    insertionSort_pequeno(vetor, 20, &comp, &desloc);
     gettimeofday(&end, NULL);
-    //   for(int i=0; i<20; i++){
-    //      printf("ordenado: %d\n", vetor1[i].chave);
-    //   }
-    free(vetor_heap);
     startTime = start.tv_sec + start.tv_usec / 1000000.0L;
     endTime = end.tv_sec + end.tv_usec / 1000000.0L;
     printf("Tempo inicial = %Lf\n", startTime);
